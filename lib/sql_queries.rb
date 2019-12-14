9e820f0c4db8b8926b353ff43df0e8a272afc1b9
Justin Phillips <justinphil02@yahoo.com>
2019-12-14 23:40:36 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -35,7 +35,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
   "SELECT users.name, SUM(pledges.amount) AS total_contributions
   FROM users
   INNER JOIN pledges
-  ON pledges.user_id = users.id 
+  ON pledges.user_id = users.id
   GROUP BY users.name
   ORDER BY total_contributions ASC; "
 end
