76e4adf77eceb2bf686660edb9888e1c1f32f1be
Justin Phillips <justinphil02@yahoo.com>
2019-12-14 23:44:33 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -43,8 +43,8 @@ end
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
   "SELECT project.category, SUM(pledges.amount)
   FROM projects
-  INNER JOIN pledges 
-  ON pledges.project_id = projects.id 
+  INNER JOIN pledges
+  ON pledges.project_id = projects.id
   WHERE projects.category = 'music'
   GROUP BY projects.category;"
 end
